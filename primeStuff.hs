e664d17231a100f72255c4679997bfadd5c8871d
Dillon Huff <dillonhuff@gmail.com>
2014-01-29 10:12:19 -0800

    a bit more

--- primeStuff.hs
+++ primeStuff.hs
@@ -1,2 +1,12 @@
+import Data.List
+
 isPrime :: Int -> Bool
-isPrime a = 
+isPrime num = (length divs) == 2 && elem num divs
+	where
+		divs = divisors num
+
+divisors :: Int -> [Int]
+divisors num = filter (isDivisor num) [1..num]
+
+isDivisor :: Int -> Int -> Bool
+isDivisor num d = (mod num d) == 0
