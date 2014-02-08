2d0ce47ab71b27ac3ae985fef04d6a27ab425992
Dillon Huff <dillonhuff@gmail.com>
2014-02-08 07:14:41 -0800

    making recursive funcs to express gcd as linear combo

--- Euclid.hs
+++ Euclid.hs
@@ -8,3 +8,13 @@ euc (a, b, qs) = if r == 0
 	where
 		r = mod a b
 		q = div a b
+		
+sSeq :: Int -> [Int] -> Int
+sSeq 0 _ = 1
+sSeq 1 _ = 0
+sSeq n q = (sSeq (n-2) q) - (q !! n)*(sSeq (n-1) q)
+
+rSeq :: Int -> [Int] -> Int
+rSeq 0 _ = 0
+rSeq 1 _ = 1
+rSeq n q = (rSeq (n-2) q) - (q !! n)*(rSeq (n-1) q)
