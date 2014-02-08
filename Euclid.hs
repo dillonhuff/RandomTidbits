cbfd8aca3c553af46f64674447506df55d71fe85
Dillon Huff <dillonhuff@gmail.com>
2014-02-08 06:48:33 -0800

    euclid working

--- Euclid.hs
+++ Euclid.hs
@@ -1,3 +1,10 @@
-euclid :: Int -> Int -> (Int, [Int], [Int])
-euclid a b = if r == 0
-	then 
+euclid :: Int -> Int -> (Int, Int, [Int])
+euclid a b = euc (a, b, [])
+
+euc :: (Int, Int, [Int]) -> (Int, Int, [Int])
+euc (a, b, qs) = if r == 0
+	then (a, b, qs)
+	else euc (b, r, q:qs)
+	where
+		r = mod a b
+		q = div a b
